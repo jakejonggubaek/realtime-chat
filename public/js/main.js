dbe0f2de6a099b086a7797e7923b79789eddeca5
jakejonggubaek <jakejonggubaek@gmail.com>
2021-05-10 20:19:22 -0400

    finished it to work at least

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,70 @@
 const chatForm = document.getElementById('chat-form');
-const chatMsg = document.getElementsByClassName('chat-messages');
+const chatMsg = document.querySelector('.chat-messages');
+
+// Get username & Room from URL
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
 
 const socket = io();
 
+//Join Chatroom
+socket.emit('joinRoom', {username, room});
+
+// Get room and users
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    outputUsers(users);
+})
+
+//message from server
 socket.on('message', message => {
-    console.log(message);
+    outputMessage(message);
+
+    //scroll down
+    chatMsg.scrollTop = chatMsg.scrollHeight;
 });
 
 //Message submit
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    //Get message text 
     const msg = e.target.elements.msg.value;
     
-    const msgEl = document.createElement('div');
-    msgEl.innerHTML = msg;
-    msgEl.classList.add('message');
-    console.log(msgEl);
-    chatMsg[0].appendChild(msg);
-    
+    //Emit message to server
+    socket.emit('chatMessage', msg);
+
+    //clear input box
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 })
+
+const outputMessage = (msgObj) => {
+    const divEl = document.createElement('div');
+    divEl.classList.add('message');
+    divEl.innerHTML = 
+        `
+            <p class="meta">${msgObj.username}
+                <span>${msgObj.time}</span></p>
+            <p class="text">
+                ${msgObj.text}
+            </p>
+        `;
+    chatMsg.appendChild(divEl);
+}
+
+// Add room name to DOM
+const outputRoomName = (room) => {
+    const roomName = document.getElementById('room-name');
+    roomName.innerText = room;
+};
+
+const outputUsers = (users) => {
+    const userList = document.getElementById('users');
+
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
+    
+};
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,18 +12,46 @@ const io = socketio(server);
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'JunoBot';
+
 //run when clients connect
 io.on('connection', socket => {
-    
-    // Welcome to current user
-    socket.emit('message', 'Welcome to Junocord');
+    socket.on('joinRoom', ({username, room})=>{
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
 
-    //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined chat');
+        // Welcome to current user
+        socket.emit('message', formatMessage(botName, 'Welcome bitches!'));
+
+        //Broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined chat`));
+
+        //Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+    });
+    
+    //Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    })
 
     //Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left chat');
+        const user = userLeave(socket.id);
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left chat`));
+        
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        
+        }
     });
 });
 
